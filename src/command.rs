003bc037dd8d1d83565a54ea3c4794d4346d8a58
Danail Delsizov <ddelsizov@gmail.com>
2024-01-28 12:14:56 +0200

    replace unwrap methods

--- src/command.rs
+++ src/command.rs
@@ -2,7 +2,7 @@ use std::net::TcpStream;
 use ssh::LocalSession;
 
 pub fn run(session: &mut LocalSession<TcpStream>, command: &String) {
-    let exec = session.open_exec().unwrap();
-    let vec: Vec<u8> = exec.send_command(command).unwrap();
-    println!("{}", String::from_utf8(vec).unwrap());
+    let exec = session.open_exec().expect("Could not establish an SSH session with remote host");
+    let vec: Vec<u8> = exec.send_command(command).expect("Error during command execution");
+    println!("{}", String::from_utf8(vec).expect("Could not retrieve output from remote command"));
 }
